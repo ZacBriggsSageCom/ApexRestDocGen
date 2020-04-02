479737d32489c542a8484fbd7589efbf57f399d6
ZacBriggsSageCom <zac.briggs@sage.com>
2020-04-02 15:24:01 +0100

    initial commit

---
+++ RestEndpoint.h
@@ -0,0 +1,48 @@
+#define RESTENDPOINT_H
+
+#include <string>
+#include <map>
+#include <vector>
+#include <iostream>
+
+struct rest_endpoint
+{
+    std::string httpMethod;
+    std::string method;
+    std::string description;
+};
+
+class RestEndpoint
+{
+public:
+    const std::string ADDRESS_TAG = "@RestResource";
+    const std::string GET_TAG = "@HttpGet";
+    const std::string POST_TAG = "@HttpPost";
+    const std::string PUT_TAG = "@HttpPut";
+    const std::string PATCH_TAG = "@HttpPatch";
+    const std::string DELETE_TAG = "@HttpDelete";
+
+    std::string *fs;
+    std::string filename;
+    std::map<std::string, int> mapToIndex;
+    std::vector<rest_endpoint> endpoints;
+    std::string address;
+    bool isEndpoint;
+
+    RestEndpoint(std::string &file, std::string &fileName);
+
+    void print();
+
+private:
+    void setFile(std::string file);
+
+    void populateAddress();
+
+    void populateEndpoints();
+
+    std::string getTagMethod(int tagIndex);
+
+    std::string getTagComment(int tagIndex);
+
+    void setTagsIndex();
+};
